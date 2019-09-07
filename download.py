df9abb7d33ebed17cce7b03d8f4e87e0d630f1e6
X360pro <43405739+X360pro@users.noreply.github.com>
2019-09-07 22:18:19 +0530

    Update download.py

--- download.py
+++ download.py
@@ -1,10 +1,12 @@
 import requests, zipfile, io, subprocess
-import shutil
+import shutil,os
 
 zip_file_url = "https://ai-hackathon-upload.s3.ap-south-1.amazonaws.com/public/data.zip"
 r = requests.get(zip_file_url)
 z = zipfile.ZipFile(io.BytesIO(r.content))
 z.extractall()
-
+os.chdir('/watersheep/')
+os.mkdir('clips')
+os.chdir('/')
 shutil.move("/*.wav", "/watersheep/clips/")
 
