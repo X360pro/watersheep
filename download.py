a9329d65fde66954c2ef5606728b8fe14a6a2b8e
X360pro <43405739+X360pro@users.noreply.github.com>
2019-09-08 01:05:09 +0530

    Update download.py

--- download.py
+++ download.py
@@ -1,6 +1,10 @@
 import requests, zipfile, io, subprocess
 import shutil,os
+from glob import glob
 
+os.chdir('/')
+os.mkdir('zip')
+os.chdir('/zip')
 zip_file_url = "https://ai-hackathon-upload.s3.ap-south-1.amazonaws.com/public/data.zip"
 r = requests.get(zip_file_url)
 z = zipfile.ZipFile(io.BytesIO(r.content))
@@ -8,5 +12,7 @@ z.extractall()
 os.chdir('/watersheep/')
 os.mkdir('clips')
 os.chdir('/')
-shutil.move("/*.wav", "/watersheep/clips/")
-
+files = glob('/zip/*.wav')
+for f in files:
+  subprocess.call(['mv',f,'/watersheep/clips'])
+os.chdir('/')
